2beda5c8ec9546f8d72397e3cb31b503e8fb38ab
abundrew <logunov@hotmail.com>
2016-05-14 22:14:00 -0400

    day 2

---
+++ contests/five-days-of-game-theory/poker_nim.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/day-2-poker-nim
+/// </summary>
+
+class Solution7
+{
+    static void Main(String[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+        for (int t = 0; t < T; t++)
+        {
+            Console.ReadLine();
+            int[] a = Console.ReadLine().Trim().Split(' ').Select(p => int.Parse(p)).ToArray();
+
+            int nimsum = 0;
+            foreach (int x in a) nimsum ^= x;
+
+            Console.WriteLine(nimsum == 0 ? "Second" : "First");
+        }
+    }
+}
