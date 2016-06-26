1115992a0d4ef8fe9801687eea11d0b2a3bd260c
abundrew <logunov@hotmail.com>
2016-06-26 12:50:05 -0400

    2016-06-26

---
+++ contests/world_codesprint_4/EqualStacks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+/// <summary>
+/// https://www.hackerrank.com/contests/june-world-codesprint/challenges/equal-stacks
+/// </summary>
+class Solution2
+{
+    static void Main(String[] args)
+    {
+        TextReader tIn = Console.In;
+        TextWriter tOut = Console.Out;
+
+        tIn = new StringReader(@"5 3 4
+3 2 1 1 1
+4 3 2
+1 1 4 1
+");
+
+        tIn.ReadLine();
+        int[][] H = new int[3][];
+        for (int i = 0; i < 3; i++)
+        {
+            H[i] = tIn.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(p => int.Parse(p)).Reverse().ToArray();
+            for (int j = 1; j < H[i].Length; j++) H[i][j] += H[i][j - 1];
+        }
+
+        HashSet<int> A = new HashSet<int>(H[0]);
+        A.IntersectWith(H[1]);
+        A.IntersectWith(H[2]);
+        tOut.WriteLine(A.Count > 0 ? A.Max() : 0);
+
+        tIn.ReadLine();
+    }
+}
