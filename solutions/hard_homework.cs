7fc2f7b97866da82d331aa648fce9ed7d2190eba
abundrew <logunov@hotmail.com>
2016-12-05 07:53:50 -0500

    2016-12-05

---
+++ solutions/hard_homework.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+/// <summary>
+/// https://www.hackerrank.com/contests/w26/challenges/hard-homework
+/// </summary>
+class Solution6
+{
+    static void Main(String[] args)
+    {
+        TextReader tIn = Console.In;
+        TextWriter tOut = Console.Out;
+
+        tIn = new StringReader(@"3000000");
+
+        int N = int.Parse(tIn.ReadLine());
+
+        int xmax = 1;
+        int ymax = 1;
+        int zmax = N - 2;
+        double max = Math.Sin(xmax) * Math.Sin(ymax) * Math.Sin(zmax);
+
+        for (int x = 1; x <= (N - 1) / 2; x++)
+        {
+            int z = N - (x + x);
+            double sinx = Math.Sin(x);
+            double sinz = Math.Sin(z);
+            if (sinx + sinx + sinz > max)
+            {
+                max = sinx + sinx + sinz;
+                xmax = x;
+                ymax = x;
+                zmax = z;
+            }
+        }
+
+        for (int z = 1; z <= N - 2; z++)
+        {
+            double sinz = Math.Sin(z);
+            if (max - sinz >= 2) continue;
+
+            int m = N - z;
+
+            double sinm = Math.Sin(m);
+            double cosm = Math.Cos(m);
+
+            double x0 = Math.Atan2(1 - cosm, sinm);
+            double x1 = Math.Atan2(cosm - 1, -sinm);
+
+            double f0 = Math.Sin(x0) * (1 - cosm) + Math.Cos(x0) * sinm;
+            double f1 = Math.Sin(x1) * (1 - cosm) + Math.Cos(x1) * sinm;
+
+            if (f0 + sinz <= max && f1 + sinz <= max) continue;
+
+            for (int x = 1; x <= m / 2; x++)
+            {
+                int y = m - x;
+                double sinx = Math.Sin(x);
+                double siny = Math.Sin(y);
+                if (sinx + siny + sinz > max)
+                {
+                    max = sinx + siny + sinz;
+                    xmax = x;
+                    ymax = y;
+                    zmax = z;
+                }
+            }
+        }
+
+        tOut.WriteLine(max.ToString("0.000000000"));
+
+        tIn.ReadLine();
+    }
+}
---
+++ solutions/satisfactory_pairs.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+/// <summary>
+/// https://www.hackerrank.com/contests/w26/challenges/pairs-again
+/// </summary>
+class Solution5
+{
+    static int gcd(int a, int b)
+    {
+        while (b != 0) b = a % (a = b);
+        return a;
+    }
+    static void Main(String[] args)
+    {
+        TextReader tIn = Console.In;
+        TextWriter tOut = Console.Out;
+
+        tIn = new StringReader(@"300000");
+
+        int N = int.Parse(tIn.ReadLine());
+
+        long K = 0;
+
+        int[] div1 = new int[N];
+        int[] div2 = new int[N];
+        for (int n1 = 1; n1 < N; n1++)
+        {
+            int ndiv1 = 0;
+            int nr1 = (int)Math.Sqrt(n1);
+            div1[ndiv1++] = 1;
+            if (n1 > 1) div1[ndiv1++] = n1;
+            for (int d = 2; d <= nr1; d++)
+                if (n1 % d == 0)
+                {
+                    div1[ndiv1++] = d;
+                    int n1d = n1 / d;
+                    if (d != n1d) div1[ndiv1++] = n1d;
+                }
+            int n2 = N - n1;
+            int ndiv2 = 0;
+            int nr2 = (int)Math.Sqrt(n2);
+            div2[ndiv2++] = 1;
+            if (n2 > 1) div2[ndiv2++] = n2;
+            for (int d = 2; d <= nr2; d++)
+                if (n2 % d == 0)
+                {
+                    div2[ndiv2++] = d;
+                    int n2d = n2 / d;
+                    if (d != n2d) div2[ndiv2++] = n2d;
+                }
+
+            for (int i1 = 0; i1 < ndiv1; i1++)
+                for (int i2 = 0; i2 < ndiv2; i2++)
+                {
+                    int a = div1[i1];
+                    int b = div2[i2];
+
+                    if (a < b && n1 / a - b / gcd(a, b) <= 0) K++;
+                }
+        }
+
+        tOut.WriteLine(K);
+
+        tIn.ReadLine();
+    }
+}
